89be89b354af9ea74971794224981f60f278edff
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-03-05 18:07:18 -0300

    :sparkles: add: comments

--- src/hello.asm
+++ src/hello.asm
@@ -1,24 +1,25 @@
 ; nasm -f elf64 src/hello.asm
+; ld -s -o hello hello.o
 
 section .data
   msg db 'Hello World!'
-  tam equ $- msg
+  msg_length equ $- msg
 
 section .text
-
+  
   global _start
 
   _start:
 
-    mov EAX, 0x4
-    mov EBX, 0x1
-    mov ECX, msg
-    mov EDX, tam
-    int 0x80
+    mov EAX, 0x4          ; quero fazer uma saida
+    mov EBX, 0x1          ; a saida é na saida padrão
+    mov ECX, msg          ; a mensagem é essa
+    mov EDX, msg_length   ; esse é o tamannho da mensagem
+    int 0x80              ; pega tudo que eu falei e executa
 
   
       ; destino, origem EAX = 1 
     mov EAX, 0x1 ; Systema Operacional estou terminando o programa
     mov EBX, 0x0 ; SO o valor de retorno é 0 (1 = error | 0 = Success)
 
-    int 0x80 ; pega toda essa info e processa
+  hello world assembly windows  int 0x80 ; pega toda essa i  fnfo e processa
